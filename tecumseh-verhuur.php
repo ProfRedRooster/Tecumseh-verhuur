8952e797c8832d81369d60ed7d222deb29baea7a
ProfRedRooster <52485505+ProfRedRooster@users.noreply.github.com>
2024-12-01 14:29:21 +0100

    versie update voor 0.4 release

--- tecumseh-verhuur.php
+++ tecumseh-verhuur.php
@@ -8,7 +8,7 @@
  * Plugin Name:       Tecumseh Verhuur
  * Plugin URI:        https://github.com/ProfRedRooster/Tecumseh-verhuur/
  * Description:       Administratie verhuur en verhuur formulieren
- * Version:           0.0.3
+ * Version:           0.0.4
  * Author:            Rohan de Graaf
  * Author URI:        https://rohandg.nl/
  * Text Domain:       tecumseh_beheer
